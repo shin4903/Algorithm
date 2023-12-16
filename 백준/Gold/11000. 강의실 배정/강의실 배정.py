b7498254c4df0a40a33d6c01ad777a32bf04765d
shin4903 <122499781+shin4903@users.noreply.github.com>
2023-12-16 22:36:36 +0900

    [Gold V] Title: 강의실 배정, Time: 504 ms, Memory: 143824 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,22 @@
+import sys
+import heapq
+N = int(sys.stdin.readline())
+classtime = []
+room = []
+for _ in range(N):
+    s, t = map(int,input().split())
+    classtime.append([s,t])
+classtime.sort()
+
+for time in classtime:
+    if not room:
+        heapq.heappush(room,time[1])
+    elif room:
+        r = heapq.heappop(room)
+        if r <= time[0]:
+            heapq.heappush(room,time[1])
+        elif r > time[0]:
+            heapq.heappush(room,r)
+            heapq.heappush(room, time[1])
+
+print(len(room))
